74118cba1568d39f56949f2f8210d231a0c212e9
jeremysitorus02 <jeremysitorus02@gmail.com>
2020-02-17 14:12:43 +0700

    sudah di update

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -31,15 +31,50 @@ public class PageController {
 		}
 		return "hello2";
 	}
-	@RequestMapping ("/calcuconvert")
-	public String calcuconvert( 
-	@RequestParam (value = "nomor1", required = false) String nomor1,
-	@RequestParam (value = "nomor2", required = false) String nomor2,
-	Model model){
-		int hasil = Integer.parseInt(nomor1) + Integer.parseInt(nomor2);
-		model.addAttribute("nomor1", nomor1);
-		model.addAttribute("nomor2", nomor2);
-		model.addAttribute("hasil", hasil);
+	@RequestMapping("/calcuconvert")
+	public String calcuconvert(
+			@RequestParam(value ="nomor1") String nomor1, 
+			@RequestParam(value ="nomor2") String nomor2,Model model) {
+		
+		int nomor11 = Integer.parseInt(nomor1);
+		int nomor22 = Integer.parseInt(nomor2);
+		String value = "";
+		String[] angkaSatuan = {"nol","satu","dua","tiga","empat","lima","enam","tujuh","delapan","sembilan"};
+		String[] angkaBelasan = {"sepuluh","sebelas","dua belas","tiga belas","empat belas","lima belas","enam belas",
+				"tujuh belas","delapan belas","sembilan belas"};
+		
+		
+		int result = nomor11 + nomor22;
+		
+		if (result < 10) {
+			value = angkaSatuan[result];
+		}
+		else if (result < 20) {
+			int temp = result;
+			temp = temp - 10;
+			value = angkaBelasan[temp];
+			
+		}
+		else if (result < 100) {
+			int temp = result / 10;
+			int mod = result % 10;
+			if (mod == 0) {
+				value = angkaSatuan[temp] + " puluh";
+			}else {
+				value = angkaSatuan[temp] + " puluh " + angkaSatuan[mod];
+			}
+			
+		}else {
+			value = "Value out of Range";
+		}
+		
+		model.addAttribute("nomor1", nomor11);
+		model.addAttribute("nomor2", nomor22);
+		model.addAttribute("result", result);
+		model.addAttribute("word",value);
+		
 		return "calcuconvert";
+		
 	}
+	
 }
